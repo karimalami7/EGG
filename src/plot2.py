98edbab80f5cd89f40f2534d3c52b85ed649df2c
mac <mac@macwifi.local>
2017-07-06 11:12:55 +0200

    correction problem legend validity in plot2

--- src/plot2.py
+++ src/plot2.py
@@ -18,12 +18,12 @@ def plot_one(element_dict,element_id,interval,config_egg,label,configG,schema):
 	
 		f,axarr=plt.subplots(len(element_dict)+1, sharex=True)
 
-		plot_id=list()
+		plot_id=[None,None]
 		for i in range(0,interval):
 			if valid_dict[i] == "T":
-				plot_id.insert(0,axarr[0].bar(i,1,color=color_dict[0]))
+				plot_id[0]=plt.bar(i,1,color=color_dict[0])
 			else:
-				plot_id.insert(1,axarr[0].bar(i,1,color=color_dict[1]))
+				plot_id[1]=plt.bar(i,1,color=color_dict[1])
 
 			axarr[0].legend(plot_id,["T","F"])
 			axarr[0].set_yticks([])
@@ -92,15 +92,15 @@ def plot_one(element_dict,element_id,interval,config_egg,label,configG,schema):
 
 	if len(element_dict) == 0 : ### because axarr doesnot support set_yticks when there is only one plot
 
-		plot_id=list()
+		plot_id=[None,None]
 		for i in range(0,interval):
 
 			if valid_dict[i] == "T":
-				plot_id.insert(0,plt.bar(i,1,color=color_dict[0]))
+				plot_id[0]=plt.bar(i,1,color=color_dict[0])
 			else:
-				plot_id.insert(1,plt.bar(i,1,color=color_dict[1]))
+				plot_id[1]=plt.bar(i,1,color=color_dict[1])
 			
-			plt.legend(plot_id,["T","F"])
+			plt.legend(plot_id,("T","F"))
 			plt.yticks([])
 			plt.title("Validity of "+configG["validity"][label]["type"]+" "+element_id+" of type "+label)
 
