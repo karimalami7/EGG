0f27239ac45e0d9495a5d7d99c9af10b1af4fb41
radu <radu.ciucanu@uca.fr>
2017-07-10 13:42:16 +0200

    touch historical reachability plots

--- demo/system_evaluation/plot.py
+++ demo/system_evaluation/plot.py
@@ -8,24 +8,30 @@ import matplotlib.pyplot as plt
 N = 3
 
 
+
 ind = np.arange(N)  # the x locations for the groups
 width = 0.35       # the width of the bars
 
 fig, ax = plt.subplots()
 
+#plt.grid()
+
 sparql_values =  (60.1594,658.7458,1000)
-rects1 = ax.bar(ind, sparql_values, width, color='r')
+rects1 = ax.bar(ind + width, sparql_values, width, color='r')
 
 DBFS_values = (76.2883,111.2985,165.9069)
 
-rects2 = ax.bar(ind + width, DBFS_values, width, color='y')
+rects2 = ax.bar(ind, DBFS_values, width, color='y')
+
+oom_values = (0, 0, 2000)
+rects3 = ax.bar(ind + width, oom_values, width, color='b')
 
 ax.set_ylabel('Time')
-ax.set_title('Sparql vs D-BFS for 100kn and 500ke')
+ax.set_title('Historical Reachability Queries: Disjunctive-BFS vs SPARQL\n Graph of size 100K nodes, 500K edges; Fixed query size=10')
 ax.set_xticks(ind + width / 2)
-ax.set_xticklabels(('10s', '100s', '1000s'))
+ax.set_xticklabels(('10 snapshots\nInterval=[0,9]', '100 snapshots\nInterval=[45,54]', '1000 snapshots\nInterval=[495,504]'))
 
-ax.legend((rects1[0], rects2[0]), ('Sparql', 'D-BFS'))
+ax.legend((rects2[0], rects1[0], rects3[0]), ('Disjunctive-BFS', 'SPARQL', 'SPARQL \'out of memory\' exception'))
 
 plt.savefig("exp1.png")
 plt.savefig("exp1.pdf")
@@ -43,18 +49,18 @@ width = 0.35       # the width of the bars
 fig, ax = plt.subplots()
 
 sparql_values =  (633.2873,634.9834,633.4736,635.0977)
-rects1 = ax.bar(ind, sparql_values, width, color='r')
+rects1 = ax.bar(ind + width, sparql_values, width, color='r')
 
 DBFS_values = (47.8177,44.993,32.4027,33.1677)
 
-rects2 = ax.bar(ind + width, DBFS_values, width, color='y')
+rects2 = ax.bar(ind, DBFS_values, width, color='y')
 
 ax.set_ylabel('Time')
-ax.set_title('Sparql vs D-BFS for 100kn and 500ke')
+ax.set_title('Historical Reachability Queries: Disjunctive-BFS vs SPARQL\n Graph of size 100K nodes, 500K edges; Fixed # of snapsots=100')
 ax.set_xticks(ind + width / 2)
-ax.set_xticklabels(('i = 50', 'i = 45->54', 'i = 25->74', 'i = 0->99'))
+ax.set_xticklabels(('Interval=[50,50]', 'Interval=[45,54]', 'Interval=[25,74]', 'Interval=[0,99]'))
 
-ax.legend((rects1[0], rects2[0]), ('Sparql', 'D-BFS'))
+ax.legend((rects2[0], rects1[0]), ('Disjunctive-BFS', 'SPARQL'))
 
 plt.savefig("exp2.png")
 plt.savefig("exp2.pdf")
