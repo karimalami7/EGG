595842d894942d44690713909d2b069aa8b90102
karim alami <alami.karim7@gmail.com>
2017-07-17 17:48:12 +0200

    up scalability

--- demo/scalability/plot_scal.py
+++ demo/scalability/plot_scal.py
@@ -34,16 +34,20 @@ with open (file+'.txt','r') as f:
 plt.plot(x,y,"bo-")
 
 if file_list[2] == "interval" :
-	plt.xlabel("Size", fontsize=14, color='black')
+	plt.xlabel("# of graph nodes", fontsize=14, color='black')
 else:
-	plt.xlabel("Interval", fontsize=14, color='black')
+	plt.xlabel("# of graph snapshots", fontsize=14, color='black')
 
 		
 plt.ylabel('Time in seconds', fontsize=14, color='black')
 plt.xscale("log", nonposx='clip')
 plt.yscale("log", nonposy='clip')
 plt.grid()
-plt.title(file_list[5]+" schema: "+file_list[2]+" set to "+file_list[4])
+if file_list[2] == "interval" :
+	plt.title(file_list[5]+" schema: "+"# of graph snapshots"+" set to "+file_list[4])
+else :
+	plt.title(file_list[5]+" schema: "+"# of graph nodes"+" set to "+file_list[4])
+
 plt.savefig(file_list[2]+"_"+file_list[5]+".png")
 plt.clf()
 
