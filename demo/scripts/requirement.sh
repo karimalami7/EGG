497f68e46a79bba2afef8946079258d68327371d
karim alami <alami.karim7@gmail.com>
2017-07-18 15:23:37 +0200

    up requirement

--- demo/scripts/requirement.sh
+++ demo/scripts/requirement.sh
@@ -1,5 +1,6 @@
 #####check gcc compiler
 if ! [ -x "$(command -v g++)" ]; then
+	echo 'apt-get install gcc-5 g++-5'
 	apt-get install gcc-5 g++-5
 else 
 	echo 'g++ exists'
@@ -8,23 +9,29 @@ fi
 
 #####check python
 if ! [ -x "$(command -v python)" ]; then
-	apt-get install sudo apt-get install python2.7
+	echo 'apt-get install python2.7'
+	apt-get  install python2.7
 else 
 	echo 'python exists'
 fi
 
+echo 'apt-get install python-tk'
+apt-get install python-tk
 
 #####clone gmark project branch gmark_for_egg
+echo 'we clone gMark'
 git clone -b gmark_for_egg  https://github.com/karimalami7/gmark.git
 mv gmark ../../
 #####compile it 
 cd ../../gmark/src
+echo 'we compile gMark'
 make
 
-apt-get install python-tk
+
 
 ####check pip
 if ! [ -x "$(command -v pip)" ]; then
+	echo 'we install pip'
 	apt-get install python-pip python-dev build-essential
 	pip install --upgrade pip
 	pip install --upgrade virtualenv
@@ -41,5 +48,5 @@ cd ../../demo/scripts
 #source ~/.local/bin/virtualenvwrapper.sh
 #mkvirtualenv egg
 #workon egg
-
+echo 'we install requirements of EGG'
 pip install -r requirement.txt
